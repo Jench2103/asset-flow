8a6f6e22f26a240d10a2f4bde56894c47a6ca984
Jen-Chien Chang <jench2103@gmail.com>
2025-10-06 14:24:51 +0800

    Initial Commit

---
+++ AssetFlow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AssetFlow
+//
+//  Created by Jen-Chien Chang on 2025/10/6.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
